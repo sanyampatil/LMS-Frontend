766ab37ef61aba67b6d6340598d8da198ae7d0f7
sanyam <patilsanyam869@gmail.com>
2023-11-04 22:03:49 +0530

    update the eslint setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {Route,Routes}  from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    lms
+    <Routes>
+    <Route path='/' element={<home/>}></Route>
+    
+      
+    </Routes>
+    
 
     </>
   )

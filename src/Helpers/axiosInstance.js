575e33b361af00ab8aa9a3fa0c1e3b0d18b6a8b9
sanyam <patilsanyam869@gmail.com>
2023-11-04 22:42:47 +0530

    setup of aasios instance done

---
+++ src/Helpers/axiosInstance.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const BASE_URL="http://localhost:5014/api/vi";
+
+const axiosInstance = axios.create()
+axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.withCredentials= true;

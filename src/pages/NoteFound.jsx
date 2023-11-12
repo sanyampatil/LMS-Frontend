56ebd7c8188b2517078871394d87668c452a4961
sanyam <patilsanyam869@gmail.com>
2023-11-12 16:45:46 +0530

    Added 404 Not Found Page

--- src/pages/NoteFound.jsx
+++ src/pages/NoteFound.jsx
@@ -7,7 +7,16 @@ function NotFound() {
             <h1 className="text-9xl font-extrabold text-white tracking-widest">
                 404
             </h1>
-            
+            <div className="bg-black text-white px-2 text-sm rounded rotate-12 absolute">
+                Page not found ...
+            </div>
+            <button className="mt-5">
+                <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-yellow-500 focus:outline-none focus:ring">
+                    <span onClick={() => navigate(-1)} className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+                        Go Back
+                    </span>
+                </a>
+            </button>
         </div>
     );
 }
